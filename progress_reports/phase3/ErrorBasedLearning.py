ffbcaebb59dc1ec500153dbf79206ec0bea6c360
sebastien-andre <37047057+sebastien-andre@users.noreply.github.com>
2024-04-14 10:58:54 -0400

    standardized naming

--- progress_reports/phase3/ErrorBasedLearning.py
+++ progress_reports/phase3/ErrorBasedLearning.py
@@ -1,3 +1,5 @@
+# Logistic Regression Model
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
--- progress_reports/phase3/SimilarityBasedLearning.py
+++ progress_reports/phase3/SimilarityBasedLearning.py
@@ -1,3 +1,5 @@
+# K Nearest Neighbors Model
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
